f88a192346a69097c3eb61ea26894215258d6a3d
RobinS-S <16294492+RobinS-S@users.noreply.github.com>
2023-02-15 23:22:59 +0100

    [ADD] Database seeding, Swagger and application user roles

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,35 +1,65 @@
 using FilmFlow.Server.Data;
-using FilmFlow.Server.Models;
+using FilmFlow.Server.Data.Models;
+using FilmFlow.Server.Swagger;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace FilmFlow
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.Configure<Config>(builder.Configuration)
+                .AddScoped(sp => sp.GetRequiredService<IOptionsSnapshot<Config>>().Value);
+
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseMySql(connectionString, new MySqlServerVersion(new Version())));
 
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
 
-            builder.Services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+            builder.Services
+                .AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerGenOptions>();
+
+            builder.Services.AddIdentityServer(options =>
+            {
+                if(builder.Environment.IsDevelopment())
+                {
+                    options.Events.RaiseErrorEvents = true;
+                    options.Events.RaiseInformationEvents = true;
+                    options.Events.RaiseFailureEvents = true;
+                    options.Events.RaiseSuccessEvents = true;
+                }
+            }).AddApiAuthorization<ApplicationUser, ApplicationDbContext>(o =>
+            {
+                if (builder.Environment.IsDevelopment()) o.Clients[0].RedirectUris.Add("/swagger/oauth2-redirect.html");
+            });
 
             builder.Services.AddAuthentication()
                 .AddIdentityServerJwt();
 
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
+            builder.Services.AddControllers();
+
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Services.AddSwaggerGen();
+            }
 
             var app = builder.Build();
 
@@ -56,6 +86,20 @@ namespace FilmFlow
             app.UseIdentityServer();
             app.UseAuthorization();
 
+            await DatabaseSeeder.SeedDatabase(app.Services); // Ensure the roles and a default user exists
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(setup =>
+                {
+                    setup.SwaggerEndpoint("/swagger/v1/swagger.json", "Version 1.0");
+                    setup.OAuthClientId("FilmFlow.Client");
+                    setup.OAuthAppName("FilmFlow API");
+                    setup.OAuthScopeSeparator(" ");
+                    setup.OAuthUsePkce();
+                });
+            }
 
             app.MapRazorPages();
             app.MapControllers();
