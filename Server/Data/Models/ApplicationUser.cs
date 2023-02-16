20636e53fb08cef7b5cc372cebae67a60f59fef4
RobinS-S <16294492+RobinS-S@users.noreply.github.com>
2023-02-17 00:47:03 +0100

    [ADD] Data folder with entities and migrations that wasn't committed due to mistake in gitignore

---
+++ Server/Data/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FilmFlow.Server.Data.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public bool IsKioskUser { get; set; }
+    }
+}
