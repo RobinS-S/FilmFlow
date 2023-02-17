40f729dfdad1a1aabdf778bbbc6a4d07251bad32
RobinS-S <16294492+RobinS-S@users.noreply.github.com>
2023-02-17 22:45:46 +0100

    [#25] [ADD] Seeding data to demo our deliverables

--- API/Data/DatabaseSeeder.cs
+++ API/Data/DatabaseSeeder.cs
@@ -1,8 +1,8 @@
 ﻿using FilmFlow.Server.Auth;
+using FilmFlow.Server.Data.Enums;
 using FilmFlow.Server.Data.Models;
 using FilmFlow.Server.Misc;
 using Microsoft.AspNetCore.Identity;
-using System.Data;
 
 namespace FilmFlow.Server.Data
 {
@@ -46,6 +46,107 @@ namespace FilmFlow.Server.Data
                 var isAdmin = await userManager.IsInRoleAsync(user, Roles.Admin);
                 if (!isAdmin) await userManager.AddToRoleAsync(user, Roles.Admin);
             }
+
+            await SeedSampleData(dbContext);
+        }
+
+        private static async Task SeedSampleData(ApplicationDbContext dbContext)
+        {
+            var halls = new List<CinemaHall>
+            {
+                new CinemaHall(10, 10, false, true),
+                new CinemaHall(12, 12, true, true),
+                new CinemaHall(8, 8, false, true)
+            };
+
+            var movies = new List<Movie>
+            {
+                new Movie("Toy Story 4", "Woody and the gang are back!", new DateTime(2019, 6, 20), "Animation", 0, "English"),
+                new Movie("Joker", "The origin of the iconic villain", new DateTime(2019, 10, 4), "Drama", 16, "English"),
+                new Movie("Avengers: Endgame", "The epic conclusion to the Infinity Saga", new DateTime(2019, 4, 26), "Action", 12, "English"),
+                new Movie("Parasite", "A poor family becomes entangled with a wealthy one", new DateTime(2019, 11, 8), "Drama", 16, "Korean"),
+                new Movie("The Lion King", "A remake of the classic Disney animated film", new DateTime(2019, 7, 19), "Animation", 0, "English")
+            };
+
+            var reviews = new List<MovieReview>
+            {
+                new MovieReview(movies[0], 4, "John Doe", "A great family movie"),
+                new MovieReview(movies[1], 5, "Jane Smith", "Joaquin Phoenix gives a stunning performance"),
+                new MovieReview(movies[2], 4, "Bob Johnson", "A fitting conclusion to the Marvel Cinematic Universe"),
+                new MovieReview(movies[3], 5, "Alice Brown", "A brilliantly crafted film with a powerful message"),
+                new MovieReview(movies[4], 3, "Jack White", "A faithful adaptation, but lacks the charm of the original")
+            };
+
+            var shows = new List<CinemaShow>();
+            foreach (var hall in halls)
+            {
+                foreach (var movie in movies)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var start = DateTime.Today.AddDays(i).AddHours(10);
+                        var end = start.AddHours(2);
+                        shows.Add(new CinemaShow(start, end, movie, hall));
+                    }
+                }
+            }
+
+            var reservations = new List<Reservation>
+            {
+                // Reservations for show 1 (cinema hall 1)
+                new Reservation(shows[0], null, true, TarriffType.NORMAL, 1, 1),
+                new Reservation(shows[0], null, false, TarriffType.CHILDREN, 2, 2),
+    
+                // Reservations for show 2 (cinema hall 2)
+                new Reservation(shows[1], null, true, TarriffType.NORMAL, 3, 3),
+                new Reservation(shows[1], null, false, TarriffType.CHILDREN, 4, 4),
+    
+                // Reservations for show 3 (cinema hall 3)
+                new Reservation(shows[2], null, true, TarriffType.NORMAL, 5, 5),
+                new Reservation(shows[2], null, false, TarriffType.CHILDREN, 6, 6),
+    
+                // Reservations for show 4 (cinema hall 1)
+                new Reservation(shows[3], null, true, TarriffType.NORMAL, 7, 7),
+                new Reservation(shows[3], null, false, TarriffType.CHILDREN, 8, 8),
+    
+                // Reservations for show 5 (cinema hall 2)
+                new Reservation(shows[4], null, true, TarriffType.NORMAL, 9, 9),
+                new Reservation(shows[4], null, false, TarriffType.CHILDREN, 10, 10),
+    
+                // Reservations for show 6 (cinema hall 3)
+                new Reservation(shows[5], null, true, TarriffType.NORMAL, 11, 11),
+                new Reservation(shows[5], null, false, TarriffType.CHILDREN, 12, 12),
+    
+                // Reservations for show 7 (cinema hall 1)
+                new Reservation(shows[6], null, true, TarriffType.NORMAL, 13, 13),
+                new Reservation(shows[6], null, false, TarriffType.CHILDREN, 14, 14),
+    
+                // Reservations for show 8 (cinema hall 2)
+                new Reservation(shows[7], null, true, TarriffType.NORMAL, 15, 15),
+                new Reservation(shows[7], null, false, TarriffType.CHILDREN, 16, 16),
+    
+                // Reservations for show 9 (cinema hall 3)
+                new Reservation(shows[8], null, true, TarriffType.NORMAL, 17, 17),
+                new Reservation(shows[8], null, false, TarriffType.CHILDREN, 18, 18),
+    
+                // Reservations for show 10 (cinema hall 1)
+                new Reservation(shows[9], null, true, TarriffType.NORMAL, 19, 19),
+                new Reservation(shows[9], null, false, TarriffType.CHILDREN, 20, 20)
+            };
+
+            var random = new Random();
+            foreach (Reservation r in reservations)
+            {
+                r.Ticket = random.Next(100) > 70 ? new ShowTicket("Kiosk") : new ShowTicket("Kassiere Anna");
+            }
+
+            dbContext.CinemaHalls.AddRange(halls);
+            dbContext.Movies.AddRange(movies);
+            dbContext.MovieReviews.AddRange(reviews);
+            dbContext.CinemaShows.AddRange(shows);
+            dbContext.Reservations.AddRange(reservations);
+
+            await dbContext.SaveChangesAsync();
         }
     }
 }
