5a81bba03e73a02709c66540f4fa96ad01ef93d9
Julian Bos <50797550+julianbos@users.noreply.github.com>
2023-03-11 01:07:37 +0100

    WIP

---
+++ API/Data/Configuration/SocialEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FilmFlow.API.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FilmFlow.API.Data.Configuration
+{
+    public class SocialEntityTypeConfiguration
+        : IEntityTypeConfiguration<Social>
+    {
+        public void Configure(EntityTypeBuilder<Social> builder)
+        {
+            builder.Property(m => m.SocialName)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.Property(m => m.Url)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(m => m.Icon)
+				.HasMaxLength(255);
+
+        }
+	}
+}
