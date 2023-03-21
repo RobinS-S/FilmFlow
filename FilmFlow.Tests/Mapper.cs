becb09b1a3480a8a3ee5f7f0f225a3a27aaa6474
RobinS-S <16294492+RobinS-S@users.noreply.github.com>
2023-03-21 19:39:07 +0100

    [ADD] More unit tests that have actual logic in them

---
+++ FilmFlow.Tests/Mapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FilmFlow.Application;
+
+namespace FilmFlow.Tests
+{
+	public class Mapper
+	{
+		public static IMapper CreateMockMapper()
+		{
+			var mockMapper = new MapperConfiguration(cfg =>
+			{
+				cfg.AddProfile(new MappingProfiles());
+			});
+			return mockMapper.CreateMapper();
+		}
+	}
+}
---
+++ FilmFlow.Tests/SocialsMappingTest.cs
@@ -0,0 +1,26 @@
+﻿using FilmFlow.Application;
+using FilmFlow.Domain.Entities;
+using FilmFlow.Shared.Dto;
+
+namespace FilmFlow.Tests
+{
+    public class SocialsMappingTest
+    {
+        [Fact]
+        public void TestSocialPropertyMapping()
+        {
+	        var mapper = Mapper.CreateMockMapper();
+
+            const string socialName = "Facebook";
+            const string socialUrl = "https://www.facebook.com";
+            const string icon = "https://www.facebook.com/avans/?locale=nl_NL";
+
+            var socialEntity = new Social(socialName, socialUrl, icon);
+            var socialDto = mapper.Map<SocialDto>(socialEntity);
+
+            Assert.Equal(socialName, socialDto.SocialName);
+            Assert.Equal(socialUrl, socialDto.Url);
+            Assert.Equal(icon, socialDto.Icon);
+        }
+    }
+}
